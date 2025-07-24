3310107463dfc5ba886a8a08e4f58fb004382af8
HP <HP@LAPTOP-G30UVCE8>
2025-07-24 19:32:49 +0530

    day4

--- Core_Java/src/com/tnsif/day4/secondpackage/Executor.java
+++ Core_Java/src/com/tnsif/day4/secondpackage/Executor.java
@@ -1,4 +1,4 @@
-package com.tnif.day4.secondpackage;
+package com.tnsif.day4.secondpackage;
 import com.tnsif.day4.firstpackage.Base;
 public class Executor {
 	public static void main(String[] args) {
