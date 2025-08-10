b5e022ef04a8c11d7292db21489e134f0e02b25d
HP <HP@LAPTOP-G30UVCE8>
2025-08-10 19:26:59 +0530

    day 25

--- Core_Java/src/com/tnsif/daytwentyfive/basictestcases/FirstTestDemo.java
+++ Core_Java/src/com/tnsif/daytwentyfive/basictestcases/FirstTestDemo.java
@@ -1,5 +1,11 @@
+//Program to demonstrate first test case
 package com.tnsif.daytwentyfive.basictestcases;
 
-public class FirstTestDemo {
+import org.junit.jupiter.api.Test;
 
+public class FirstTestDemo {
+	@Test
+	void firstTest() {
+		System.out.println("Hello");
+	}
 }
--- Core_Java/src/com/tnsif/daytwentyfive/basictestcases/testsuiteclasses/ClassATest.java
+++ Core_Java/src/com/tnsif/daytwentyfive/basictestcases/testsuiteclasses/ClassATest.java
@@ -1,5 +1,14 @@
+//test case for development and production
 package com.tnsif.daytwentyfive.basictestcases.testsuiteclasses;
 
-public class ClassATest {
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
+public class ClassATest {
+	@Test
+	@Tag("development")
+	@Tag("production")
+	void testCaseA(TestInfo testInfo) { //run in all environments
+	}
 }
