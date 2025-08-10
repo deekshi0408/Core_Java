747d02cf60f58b2ef41b3835aada1aef8523a50f
HP <HP@LAPTOP-G30UVCE8>
2025-08-10 14:54:53 +0530

    day 21

--- Core_Java/src/com/tnsif/daytwentyone/userdefinedannotation/SmartTV.java
+++ Core_Java/src/com/tnsif/daytwentyone/userdefinedannotation/SmartTV.java
@@ -1,5 +1,19 @@
 package com.tnsif.daytwentyone.userdefinedannotation;
 
-public interface SmartTV {
+//Program to Demonstrate User Define Annotation - SmartTV
 
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SmartTV {
+	String os();
+
+	int width();
+
+	int height();
 }
